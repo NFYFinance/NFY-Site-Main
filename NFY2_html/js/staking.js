73af2c3eb3f4f16f6ddfaad649a2d2eaf06e7407
JExtor <JeffreyJExtor@gmail.com>
2020-10-25 15:46:28 -0400

    Working on web3 everything is looking good

--- NFY2_html/js/staking.js
+++ NFY2_html/js/staking.js
@@ -1,33 +1,234 @@
 var web3;
 web3 = new Web3(Web3.givenProvider);
-var contractInstance;
-var LPTokens
+var nfyToken;
+var LPTokens;
 var accounts;
+var nfyStaking;
+var nfyStakingNFT;
+var lpStaking;
+var lpStakingNFT;
+
+var lpStakingAddress = "0x33FB69137C5E3bc7966f8B61763Bee3260928624";
+var nfyStakingAddress = "0x6d9575c898d8224941c9EfcE3cB77D72EB2d1ec7";
+
+var maxAllowance = 1157920892373161954235709850086879078532699846656405;
 
 
 $(document).ready(async function() {
 
    connect();
 
-   console.log(contractInstance);
+   console.log(nfyToken);
 
    $(".connect_button").click(connect);
+   $("#submit-nfy-stake").click(stakeNfy);
+   $("#submit-lp-stake").click(stakeLp);
 
 })
 
+function stakeNfy() {
 
+    LPTokens.methods.allowance(accounts[0], lpStakingAddress).call().then(function(res){
+        if(res == 0){
+            LPTokens.methods.approve(lpStakingAddress, (BigInt(maxAllowance)).toString()).send()
 
-function alert () {
+            .on("transactionHash", function(hash){
+                console.log(hash);
+            })
 
+            .on("confirmation", function(confirmationNr){
+                console.log(confirmationNr);
+            })
+
+            .on("receipt", function(receipt){
+                console.log(receipt);
+            })
+        }
+
+        else {
+            var stakeVal = $("#input-lp-stake").val();
+
+            var stake = web3.utils.toWei(stakeVal, "ether");
+
+            lpStaking.methods.stakeLP(stake).send()
+
+            .on("transactionHash", function(hash){
+                console.log(hash);
+            })
+
+            .on("confirmation", function(confirmationNr){
+                console.log(confirmationNr);
+            })
+
+            .on("receipt", function(receipt){
+                console.log(receipt);
+            })
+        }
+    })
+
+    /*var stakeVal = $("#input-nfy-stake").val();
+
+    var stake = web3.utils.toWei(stakeVal, "ether");
+
+    nfyStaking.methods.stakeNFY(stake).send()
+
+    .on("transactionHash", function(hash){
+        console.log(hash);
+    })
+
+    .on("confirmation", function(confirmationNr){
+        console.log(confirmationNr);
+    })
+
+    .on("receipt", function(receipt){
+        console.log(receipt);
+    })*/
 }
 
 function getNfyBalance() {
-
     // Get NFY balance
-    contractInstance.methods.balanceOf(accounts[0]).call().then(function(res){
-    res = res / 1000000000000000000;
+    nfyToken.methods.balanceOf(accounts[0]).call().then(function(res){
+        res = res / 1000000000000000000;
 
-        $("#nfy-balance").text("YOUR NFY BALANCE : " + res);
+        $("#nfy-balance").text(res);
+    })
+}
+
+function getTotalNfyStaked() {
+    nfyStaking.methods.totalStaked().call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#total-nfy-staked").text(res + " NFY");
+    })
+}
+
+function getUserStakedNfy() {
+    nfyStaking.methods.getTotalBalance(accounts[0]).call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#nfy-staked").text(res);
+    })
+}
+
+function getUserNfyNfts() {
+    nfyStakingNFT.methods.balanceOf(accounts[0]).call().then(function(res){
+        $("#total-nfy-nfts").text(res);
+    })
+}
+
+function getNfyAPY() {
+    var rewardPerBlock;
+    nfyStaking.methods.getRewardPerBlock().call().then(function(rpb){
+        rewardPerBlock = rpb;
+    })
+
+    nfyStaking.methods.totalStaked().call().then(function(staked){
+        if(staked == 0){
+            $("#nfy-apy").text('0' + "%");
+        }
+
+        else{
+            var apy = rewardPerBlock * 6500 * 366 * 100 / staked;
+            $("#nfy-apy").text(apy.toFixed(2) + "%");
+        }
+    })
+}
+
+function getUserNfyRewards() {
+    nfyStaking.methods.getTotalRewards(accounts[0]).call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#nfy-rewards").text(res.toFixed(4) + " NFY");
+    })
+}
+
+function stakeLp() {
+
+    LPTokens.methods.allowance(accounts[0], lpStakingAddress).call().then(function(res){
+        if(res == 0){
+            LPTokens.methods.approve(lpStakingAddress, (BigInt(maxAllowance)).toString()).send()
+
+            .on("transactionHash", function(hash){
+                console.log(hash);
+            })
+
+            .on("confirmation", function(confirmationNr){
+                console.log(confirmationNr);
+            })
+
+            .on("receipt", function(receipt){
+                console.log(receipt);
+            })
+        }
+
+        else {
+            var stakeVal = $("#input-lp-stake").val();
+
+            var stake = web3.utils.toWei(stakeVal, "ether");
+
+            lpStaking.methods.stakeLP(stake).send()
+
+            .on("transactionHash", function(hash){
+                console.log(hash);
+            })
+
+            .on("confirmation", function(confirmationNr){
+                console.log(confirmationNr);
+            })
+
+            .on("receipt", function(receipt){
+                console.log(receipt);
+            })
+        }
+    })
+
+}
+
+function getTotalLpStaked() {
+    lpStaking.methods.totalStaked().call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#total-lp-staked").text(res + " NFY/ETH LP");
+    })
+}
+
+function getUserStakedLp() {
+    lpStaking.methods.getTotalBalance(accounts[0]).call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#lp-staked").text(res);
+    })
+}
+
+function getUserLpNfts() {
+    lpStakingNFT.methods.balanceOf(accounts[0]).call().then(function(res){
+        $("#total-lp-nfts").text(res);
+    })
+}
+
+function getLpAPY() {
+    var rewardPerBlock;
+    lpStaking.methods.getRewardPerBlock().call().then(function(rpb){
+        rewardPerBlock = rpb;
+    })
+
+    lpStaking.methods.totalStaked().call().then(function(staked){
+        if(staked == 0){
+            $("#lp-apy").text('0' + "%");
+        }
+
+        else{
+            var apy = rewardPerBlock * 6500 * 366 * 100 / staked;
+            $("#lp-apy").text(apy.toFixed(2) + "%");
+        }
+    })
+}
+
+function getUserLpRewards() {
+    lpStaking.methods.getTotalRewards(accounts[0]).call().then(function(res){
+        res = res / 1000000000000000000;
+
+        $("#lp-rewards").text(res.toFixed(4) + " NFY");
     })
 }
 
@@ -35,9 +236,9 @@ function getLPBalance() {
 
     // Get NFY balance
     LPTokens.methods.balanceOf(accounts[0]).call().then(function(res){
-    res = res / 1000000000000000000;
+        res = res / 1000000000000000000;
 
-        $("#lp-balance").text("YOUR NFY/ETH LP BALANCE : " + res);
+        $("#lp-balance").text(res);
     })
 }
 
@@ -45,8 +246,6 @@ function connected() {
     var accountsAbrv = accounts[0].slice(0,7);
     $(".connect_button").text("CONNECTED TO: " + accountsAbrv + "...");
 }
-
-
 
 async function connect() {
     try {
@@ -74,16 +273,35 @@ async function connect() {
     }
 
     accounts = await web3.eth.getAccounts();
-    contractInstance = new web3.eth.Contract(NFYAbi, "0x1cBb83EbcD552D5EBf8131eF8c9CD9d9BAB342bC", {from: accounts[0]});
-    LPTokens = new web3.eth.Contract(LPAbi, "0x146D3401B6a41122Bd318ba676A01c44cB0795E2", {from: accounts[0]});
-    console.log(contractInstance);
+    nfyToken = new web3.eth.Contract(NFYAbi, "0x1cBb83EbcD552D5EBf8131eF8c9CD9d9BAB342bC", {from: accounts[0]});
+    //LPTokens = new web3.eth.Contract(LPAbi, "0x146D3401B6a41122Bd318ba676A01c44cB0795E2", {from: accounts[0]});
+    LPTokens = new web3.eth.Contract(DEMOLPAbi, "0xeA90734284A80f016861301Bdab3C5F2F34FC33b", {from: accounts[0]});
+
+    nfyStaking = new web3.eth.Contract(NFYStakingABI, "0x6d9575c898d8224941c9EfcE3cB77D72EB2d1ec7", {from: accounts[0]});
+    nfyStakingNFT = new web3.eth.Contract(NFYStakingNFTABI, "0x4D47ad91Bb5D7ef813a4e959875811ed9B7e8c68", {from: accounts[0]});
+
+    lpStaking = new web3.eth.Contract(LPStakingABI, "0x33FB69137C5E3bc7966f8B61763Bee3260928624", {from: accounts[0]});
+    lpStakingNFT = new web3.eth.Contract(LPStakingNFTABI, "0x63F0bdab3A41B834656C0FE8e1e49db042AaD6BB", {from: accounts[0]});
+
+    console.log(nfyToken);
     console.log(accounts[0]);
 
     getNfyBalance();
     getLPBalance();
+
+    getTotalNfyStaked();
+    getUserStakedNfy();
+    getUserNfyNfts();
+    getNfyAPY();
+    getUserNfyRewards();
+
+    getTotalLpStaked();
+    getUserStakedLp();
+    getUserLpNfts();
+    getLpAPY();
+    getUserLpRewards();
+
     connected();
-
-
 }
 
 
